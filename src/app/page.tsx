431daf761d55948f70175167b31889dea1e46d52
gustavolimapereira <gu.lim@hotmail.com>
2026-02-04 22:30:06 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image"
+import Image from 'next/image';
 
 export default function Home() {
   return (
-
-    <div>
-      hello world
-    </div>
+    <>
+      <div>hello world</div>
+      <div>teste</div>
+    </>
   );
 }
